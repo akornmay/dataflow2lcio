1e2f7b9cd9b243bc488b76011d1678bcf4975f5a
akornmay <akornmay@pixsim-1.cern.ch>
2014-10-28 19:01:58 +0100

    updated the env.sh to pick up xin's lcio installation

--- env.sh
+++ env.sh
@@ -1,7 +1,7 @@
 
 #Edit this line to point towards your LCIO installation
-export LCIO=/home/mardelcourt/Documents/Cours/PHYS22MA/SummerStudent/Work/converter/lcio/v02-04-03
 
+export LCIO=/afs/cern.ch/work/x/xshi/public/ILCSOFT/v01-17-03/lcio/v02-04-02
 
 export PATH=$LCIO/tools:$LCIO/bin:$PATH
 export LD_LIBRARY_PATH=$LCIO/lib:$LD_LIBRARY_PATH
